b6c3327588f7e343324714e0f6483dfcd2925ea2
DreamingCosmos <visionaryaspiring@gmail.com>
2025-07-17 05:02:38 -0400

    Add .next/static/chunks/src_app_submit_page_tsx_0ef0cac3._.js 1752742767841

---
+++ .next/static/chunks/src_app_submit_page_tsx_0ef0cac3._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_abb8b7d3._.js",
+  "static/chunks/node_modules_60bd4518._.js"
+],
+    source: "dynamic"
+});
